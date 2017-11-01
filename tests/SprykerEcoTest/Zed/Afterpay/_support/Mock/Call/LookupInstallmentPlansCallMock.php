e21de0c033a35c7eac05e1c55513f80b8686863d
Sergey Sikachev <sergey.sikachev@spryker.com>
2017-11-01 11:20:44 +0000

    ECO-592. Remove useless use statements.

--- tests/SprykerEcoTest/Zed/Afterpay/_support/Mock/Call/LookupInstallmentPlansCallMock.php
+++ tests/SprykerEcoTest/Zed/Afterpay/_support/Mock/Call/LookupInstallmentPlansCallMock.php
@@ -2,13 +2,8 @@
 
 namespace SprykerEcoTest\Zed\Afterpay\Mock\Call;
 
-use Generated\Shared\DataBuilder\AfterpayCustomerLookupResponseBuilder;
-use Generated\Shared\DataBuilder\AfterpayInstallmentPlansRequestBuilder;
 use Generated\Shared\DataBuilder\AfterpayInstallmentPlansResponseBuilder;
-use Generated\Shared\Transfer\AfterpayCustomerLookupRequestTransfer;
 use Generated\Shared\Transfer\AfterpayInstallmentPlansRequestTransfer;
-use Generated\Shared\Transfer\AfterpayInstallmentPlansResponseTransfer;
-use SprykerEco\Zed\Afterpay\Business\Api\Adapter\ApiCall\LookupCustomerCall;
 use SprykerEco\Zed\Afterpay\Business\Api\Adapter\ApiCall\LookupInstallmentPlansCall;
 
 class LookupInstallmentPlansCallMock extends LookupInstallmentPlansCall
