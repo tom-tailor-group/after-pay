c270c47e04bbe582a071dcf6546920c1f6258d0d
Sergey Sikachev <sergey.sikachev@spryker.com>
2017-11-01 08:16:51 +0000

    Add first version of getPaymentBySalesOrderItem test.

--- tests/SprykerEcoTest/Zed/Afterpay/Business/AfterpayFacadeGetPaymentByIdSalesOrderTest.php
+++ tests/SprykerEcoTest/Zed/Afterpay/Business/AfterpayFacadeGetPaymentByIdSalesOrderTest.php
@@ -7,6 +7,38 @@
 
 namespace SprykerEcoTest\Zed\Afterpay\Business;
 
+use SprykerEco\Shared\Afterpay\AfterpayConstants;
+
 class AfterpayFacadeGetPaymentByIdSalesOrderTest extends AfterpayFacadeAbstractTest
 {
+    /**
+     * @return void
+     */
+    public function testGetPaymentByIdSalesOrder()
+    {
+        $idSalesOrder = 45;
+        $output = $this->doFacadeCall($idSalesOrder);
+        $this->doTest($output);
+    }
+
+    /**
+     * @param int $input
+     *
+     * @return \Generated\Shared\Transfer\AfterpayPaymentTransfer
+     */
+    protected function doFacadeCall($input)
+    {
+        return $this->facade->getPaymentByIdSalesOrder($input);
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\AfterpayPaymentTransfer $output
+     *
+     * @return void
+     */
+    protected function doTest($output)
+    {
+        $paymentMethod = $output->getPaymentMethod();
+        $this->assertTrue(in_array($paymentMethod, [AfterpayConstants::RISK_CHECK_METHOD_INVOICE]));
+    }
 }
